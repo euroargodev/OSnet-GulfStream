bfd3418a9a4d79164dea1931f692e9c9b8f0335c
Guillaume Maze <gmaze@ifremer.fr>
2022-02-15 16:23:06 +0100

    Update facade.py
    Should handle any input shape

--- osnet/facade.py
+++ osnet/facade.py
@@ -24,25 +24,50 @@ class osnet_proto(ABC):
         """ Show keras network summary for a model instance """
         return self.models[id].summary()
 
+    @property
+    def SDL(self):
+        return [0, 1, 2, 3, 4, 5, 6, 8, 10, 12, 14, 16, 19, 22, 26, 30, 35, 40, 45, 50, 55,
+                        60, 65, 70, 75, 80, 90, 100, 110, 120, 133, 147, 163, 180, 199, 221, 245, 271,
+                        301, 334, 371, 412, 458, 509, 565, 628, 697, 773, 857, 950, 1000]
+
+    # def _make_X(self, x):
+    #     """ create X vector """
+    #     d = 1/365
+    #     cos_week = np.cos(np.pi * 2 * d * x['dayOfYear'].data)
+    #     sin_week = np.sin(np.pi * 2 * d * x['dayOfYear'].data)
+    #     X = np.zeros([len(x['lat']), 12])
+    #     X[:,0] = x['SLA'].data
+    #     X[:,1] = x['lat'].data
+    #     X[:,2] = x['lon'].data
+    #     X[:,3] = cos_week
+    #     X[:,4] = sin_week
+    #     X[:,5] = x['MDT'].data
+    #     X[:,6] = x['UGOSA'].data
+    #     X[:,7] = x['VGOSA'].data
+    #     X[:,8] = x['UGOS'].data
+    #     X[:,9] = x['VGOS'].data
+    #     X[:,10] = x['SST'].data
+    #     X[:,11] = -x['BATHY'].data
+    #     return X
+
     def _make_X(self, x):
         """ create X vector """
-        d = 1/365
-        cos_week = np.cos(np.pi * 2 * d * x['dayOfYear'].data)
-        sin_week = np.sin(np.pi * 2 * d * x['dayOfYear'].data)
-        X = np.zeros([len(x['lat']), 12])
-        X[:,0] = x['SLA'].data
-        X[:,1] = x['lat'].data
-        X[:,2] = x['lon'].data
-        X[:,3] = cos_week
-        X[:,4] = sin_week
-        X[:,5] = x['MDT'].data
-        X[:,6] = x['UGOSA'].data
-        X[:,7] = x['VGOSA'].data
-        X[:,8] = x['UGOS'].data
-        X[:,9] = x['VGOS'].data
-        X[:,10] = x['SST'].data
-        X[:,11] = -x['BATHY'].data
-        return X
+        x = x.stack({'sampling': list(x.dims)})
+        N_samples = len(x['sampling'])
+        X = np.zeros([N_samples, 12])
+        X[:, 0] = x['SLA'].data
+        X[:, 1] = x['lat'].data
+        X[:, 2] = x['lon'].data
+        X[:, 3] = np.cos(np.pi * 2 * 1 / 365 * x['dayOfYear'].data)
+        X[:, 4] = np.sin(np.pi * 2 * 1 / 365 * x['dayOfYear'].data)
+        X[:, 5] = x['MDT'].data
+        X[:, 6] = x['UGOSA'].data
+        X[:, 7] = x['VGOSA'].data
+        X[:, 8] = x['UGOS'].data
+        X[:, 9] = x['VGOS'].data
+        X[:, 10] = x['SST'].data
+        X[:, 11] = -np.abs(x['BATHY'].data)
+        return X, x
 
     def _get_mean_std_pred(self, ensemble, X, Sm, Sstd, Tm, Tstd):
         predS = []
@@ -59,27 +84,41 @@ class osnet_proto(ABC):
             mld, axis=0)
 
     def _get_MLD_from_mask(self, mask):
-        depth_levels = [0, 1, 2, 3, 4, 5, 6, 8, 10, 12, 14, 16, 19, 22, 26, 30, 35, 40, 45, 50, 55,
-                        60, 65, 70, 75, 80, 90, 100, 110, 120, 133, 147, 163, 180, 199, 221, 245, 271,
-                        301, 334, 371, 412, 458, 509, 565, 628, 697, 773, 857, 950, 1000]
+        depth_levels = self.SDL
         mask = np.sign(0.5 - mask)
         return depth_levels[np.argmin(mask)]
+
+    # def _add_sig(self, ds):
+    #     SA = gsw.SA_from_SP(ds['psal'], ds['PRES_INTERPOLATED'], ds['lon'], ds['lat'])
+    #     CT = gsw.CT_from_t(SA, ds['temp'], ds['PRES_INTERPOLATED'])
+    #     sig = gsw.sigma0(SA, CT)
+    #     ds = ds.assign(variables={"sig": (('lat', 'PRES_INTERPOLATED'), sig.data)})
+    #     return ds
 
     def _add_sig(self, ds):
         SA = gsw.SA_from_SP(ds['psal'], ds['PRES_INTERPOLATED'], ds['lon'], ds['lat'])
         CT = gsw.CT_from_t(SA, ds['temp'], ds['PRES_INTERPOLATED'])
         sig = gsw.sigma0(SA, CT)
-        ds = ds.assign(variables={"sig": (('lat', 'PRES_INTERPOLATED'), sig.data)})
-        return ds
+        return ds.assign(variables={"sig": sig})
+
+    # def _add_maskv3(self, ds):
+    #     b = 2
+    #     b2 = 1
+    #     H = 0.5664  # For OSnet Gulf Stream, see Pauthenet et al, 2022
+    #     mask2 = np.where(ds['MLD_mask'].data < H, ds['MLD_mask'], 1)
+    #     ds = ds.assign(variables={"MLD_mask2": (('lat', 'PRES_INTERPOLATED'), mask2)})
+    #     mask3 = np.where((ds['MLD_mask'] > H) & (ds['MLD_mask'] < b2), b - ds['MLD_mask'].data, ds['MLD_mask2'].data)
+    #     ds = ds.assign(variables={"MLD_mask3": (('lat', 'PRES_INTERPOLATED'), mask3)})
+    #     return ds
 
     def _add_maskv3(self, ds):
         b = 2
         b2 = 1
         H = 0.5664  # For OSnet Gulf Stream, see Pauthenet et al, 2022
         mask2 = np.where(ds['MLD_mask'].data < H, ds['MLD_mask'], 1)
-        ds = ds.assign(variables={"MLD_mask2": (('lat', 'PRES_INTERPOLATED'), mask2)})
+        ds = ds.assign(variables={"MLD_mask2": (('sampling', 'PRES_INTERPOLATED'), mask2)})
         mask3 = np.where((ds['MLD_mask'] > H) & (ds['MLD_mask'] < b2), b - ds['MLD_mask'].data, ds['MLD_mask2'].data)
-        ds = ds.assign(variables={"MLD_mask3": (('lat', 'PRES_INTERPOLATED'), mask3)})
+        ds = ds.assign(variables={"MLD_mask3": (('sampling', 'PRES_INTERPOLATED'), mask3)})
         return ds
 
     def _post_processing_adjustment(self, ds, mask):
@@ -107,32 +146,42 @@ class osnet_proto(ABC):
         ct_out = gsw.CT_from_t(sa_out, temp_out, ds['PRES_INTERPOLATED'])
         sig_out = gsw.sigma0(sa_out, ct_out)
 
-        ds_out = ds.assign(variables={"temp_adj": (('lat', 'PRES_INTERPOLATED'), temp_out.data),
-                                      "psal_adj": (('lat', 'PRES_INTERPOLATED'), psal_out.data),
-                                      "sig_adj": (('lat', 'PRES_INTERPOLATED'), sig_out.data)})
+        ds_out = ds.assign(variables={"temp_adj": (('sampling', 'PRES_INTERPOLATED'), temp_out.data),
+                                      "psal_adj": (('sampling', 'PRES_INTERPOLATED'), psal_out.data),
+                                      "sig_adj": (('sampling', 'PRES_INTERPOLATED'), sig_out.data)})
         return ds_out
 
     def _predict(self, x, ensemble, scal_Sm, scal_Sstd, scal_Tm, scal_Tstd, scaler_input, **kwargs):
         """ Make prediction """
-        X = self._make_X(x)
+        X, y = self._make_X(x)
         X_scaled = scaler_input.transform(X)
 
         # ------------- Predict and add to dataset -------------- #
         get_MLD_from_mask_vect = np.vectorize(self._get_MLD_from_mask, signature='(k)->()')
         pred_S_mean, pred_S_std, pred_T_mean, pred_T_std, mld = self._get_mean_std_pred(ensemble, X_scaled, scal_Sm, scal_Sstd, scal_Tm, scal_Tstd)
-        x = x.assign(variables={"psal": (('lat', 'PRES_INTERPOLATED'), pred_S_mean.data)})
-        x = x.assign(variables={"temp": (('lat', 'PRES_INTERPOLATED'), pred_T_mean.data)})
-        x = x.assign(variables={"MLD_mask": (('lat', 'PRES_INTERPOLATED'), mld.data)})
-        x = x.assign(variables={"mld": (('lat'), get_MLD_from_mask_vect(x.MLD_mask))})
-        x = x.assign(variables={"psal_std": (('lat', 'PRES_INTERPOLATED'), pred_S_std.data)})
-        x = x.assign(variables={"temp_std": (('lat', 'PRES_INTERPOLATED'), pred_T_std.data)})
+
+        # x = x.assign(variables={"psal": (('lat', 'PRES_INTERPOLATED'), pred_S_mean.data)})
+        # x = x.assign(variables={"temp": (('lat', 'PRES_INTERPOLATED'), pred_T_mean.data)})
+        # x = x.assign(variables={"MLD_mask": (('lat', 'PRES_INTERPOLATED'), mld.data)})
+        # x = x.assign(variables={"mld": (('lat'), get_MLD_from_mask_vect(x.MLD_mask))})
+        # x = x.assign(variables={"psal_std": (('lat', 'PRES_INTERPOLATED'), pred_S_std.data)})
+        # x = x.assign(variables={"temp_std": (('lat', 'PRES_INTERPOLATED'), pred_T_std.data)})
+
+        y = y.assign(variables={"PRES_INTERPOLATED": (("PRES_INTERPOLATED"), self.SDL)})
+        y = y.assign(variables={"temp": (("sampling", "PRES_INTERPOLATED"), pred_T_mean)})
+        y = y.assign(variables={"temp_std": (("sampling", "PRES_INTERPOLATED"), pred_T_std)})
+        y = y.assign(variables={"psal": (("sampling", "PRES_INTERPOLATED"), pred_S_mean)})
+        y = y.assign(variables={"psal_std": (("sampling", "PRES_INTERPOLATED"), pred_S_std)})
+        y = y.assign(variables={"mld": (("sampling"), get_MLD_from_mask_vect(mld.data))})
+        y = y.assign(variables={"MLD_mask": (("sampling", "PRES_INTERPOLATED"), mld.data)})
 
         if ('adjust_mld' in kwargs and kwargs['adjust_mld']) or self.adjust_mld:
-            x = self._add_sig(x)
-            x = self._add_maskv3(x)
-            x = self._post_processing_adjustment(x, mask=x['MLD_mask3'])
+            y = y.assign(variables={"MLD_mask": (("sampling", "PRES_INTERPOLATED"), mld.data)})
+            y = self._add_sig(y)
+            y = self._add_maskv3(y)
+            y = self._post_processing_adjustment(y, mask=y['MLD_mask3'])
 
-        return x
+        return y.unstack('sampling')
 
 class osnet(osnet_proto):
     def __init__(self,
