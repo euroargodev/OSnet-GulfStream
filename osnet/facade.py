35bec61223da73dce89314b8f6df7799ba9e8f34
Guillaume Maze <gmaze@ifremer.fr>
2022-02-16 12:36:13 +0100

    Update facade.py
    - Add trace in dataset that OSnet stuff have been done ('sign')
    - Add mask to handle grid points with NaNs
    - More docstrings

--- osnet/facade.py
+++ osnet/facade.py
@@ -8,6 +8,11 @@ import gsw
 import xarray as xr
 from numba import float64, guvectorize
 import pkg_resources
+import logging
+
+
+log = logging.getLogger("osnet.facade")
+
 
 class osnet_proto(ABC):
     adjust_mld = True
@@ -24,17 +29,46 @@ class osnet_proto(ABC):
         """ Show keras network summary for a model instance """
         return self.models[id].summary()
 
+    def _sign_predictions(self, this_obj):
+        """ Add attribute to 'sign' a variable as associated with OSnet """
+        if isinstance(this_obj, xr.Dataset):
+            this_obj.attrs['OSnet'] = "This dataset has variable(s) generated with OSnet-%s model: %s" % (self.info['name'], self.info['ref'])
+        if isinstance(this_obj, xr.DataArray):
+            this_obj.attrs['OSnet'] = "This variable has been generated with OSnet-%s model: %s" % (self.info['name'], self.info['ref'])
+        return this_obj
+
+    def _is_signed(self, this_obj):
+        return 'OSnet' in this_obj.attrs
+
     @property
     def SDL(self):
+        """ Standard Depth Levels of predictions """
         return [0, 1, 2, 3, 4, 5, 6, 8, 10, 12, 14, 16, 19, 22, 26, 30, 35, 40, 45, 50, 55,
                         60, 65, 70, 75, 80, 90, 100, 110, 120, 133, 147, 163, 180, 199, 221, 245, 271,
                         301, 334, 371, 412, 458, 509, 565, 628, 697, 773, 857, 950, 1000]
 
+    def _mask_X(self, x):
+        """ Compute a mask to make the X vector full, i.e. without NaN """
+        M = list()
+        Features = ['SST', 'SLA', 'MDT']
+        for v in Features:
+            M.append(x[v].notnull())
+        mask = xr.concat(M, dim='n_features')
+        mask = mask.sum(dim='n_features')
+        mask = mask == len(Features)
+        return mask
+
     def _make_X(self, x):
         """ create X vector """
+        # Stack and mask in the input array:
         x = x.stack({'sampling': list(x.dims)})
+        self._mask = self._mask_X(x)
+        x = x.where(self._mask == 1, drop=True)
+
+        # Create [Samples, Features] array to work with:
         N_samples = len(x['sampling'])
         X = np.zeros([N_samples, 12])
+        log.debug("Osnet working with X[%i,%i] input array" % (X.shape[0], X.shape[1]))
         X[:, 0] = x['SLA'].data
         X[:, 1] = x['lat'].data
         X[:, 2] = x['lon'].data
@@ -116,7 +150,7 @@ class osnet_proto(ABC):
 
     def _predict(self, x, ensemble, scal_Sm, scal_Sstd, scal_Tm, scal_Tstd, scaler_input, **kwargs):
         """ Make prediction """
-        X, y = self._make_X(x)
+        X, y = self._make_X(x)  # y is stacked/masked x
         X_scaled = scaler_input.transform(X)
 
         # Prediction:
@@ -130,6 +164,7 @@ class osnet_proto(ABC):
         y = y.assign(variables={"psal": (("sampling", "PRES_INTERPOLATED"), pred_S_mean)})
         y = y.assign(variables={"psal_std": (("sampling", "PRES_INTERPOLATED"), pred_S_std)})
         y = y.assign(variables={"mld": (("sampling"), get_MLD_from_mask_vect(mld.data))})
+        vlist = ['temp', 'temp_std', 'psal', 'psal_std', 'mld']
 
         # Adjust Mixed layer:
         if ('adjust_mld' in kwargs and kwargs['adjust_mld']) or self.adjust_mld:
@@ -138,6 +173,22 @@ class osnet_proto(ABC):
             y = self._add_maskv3(y)
             y = self._post_processing_adjustment(y, mask=y['MLD_mask3'])
             y = y.drop_vars(['MLD_mask', 'MLD_mask2', 'MLD_mask3'])
+            vlist += ['sig', 'temp_adj', 'psal_adj', 'sig_adj']
+
+        # # Add attributes:
+        # for v in vlist:
+        #     y[v] = self._sign_predictions(y[v])
+        #
+        # # Prepare output according to input mask
+        # x = x.stack({'sampling': list(x.dims)})
+        # for v in list(set(y.data_vars) - set(x.data_vars)):
+        #     x = x.assign({v: y[v]})
+        # x = x.unstack('sampling')
+        #
+        # if (np.prod(x['SST'].shape) != self._xmask.shape[0]):
+        #     log.debug("Unravelled data not matching mask dimension, re-indexing")
+        #     mask = self._xmask.unstack()
+        #     x['SST'] = x['SST'].reindex_like(mask)
 
         return y.unstack('sampling')
 
@@ -181,17 +232,64 @@ class osnet(osnet_proto):
 
         return self
 
-    def predict(self, ds_inputs, **kwargs):
-        ds_output = self._predict(x=ds_inputs,
-                                    ensemble=self.models,
-                                    scal_Sm=self.scalers['scal_Sm'], scal_Sstd=self.scalers['scal_Sstd'],
-                                    scal_Tm=self.scalers['scal_Tm'], scal_Tstd=self.scalers['scal_Tstd'],
-                                    scaler_input=self.scalers['scaler_input'],
-                                    suffix="_TS",
-                                    **kwargs)
-        # ds_output has ds_inputs variables dimensions broadcasted, so we need to re-assign to their original shape:
-        for v in ds_inputs.data_vars:
-            if v in ds_output:
-                ds_output = ds_output.assign({v:ds_inputs[v]})
+    def predict(self, x, inplace=True, **kwargs):
+        """ Make T/S/MLD predictions
 
-        return ds_output
+        Parameters
+        ----------
+        x: :class:`xarray.DataSet`
+            Input dataset.
+            Must have at least the following coordinates:
+                - 'lon', 'lat', 'time'
+            Otherwise, we expect:
+                1. ``dayOfYear``
+                1. ``BATHY``
+                1. ``lat`` in the range: [18.125, 54.875]
+                1. ``lon`` in the range: [275.125, 334.875] or [-84.875, -25.125]
+                1. ``SST``
+                1. ``SLA``
+                1. ``UGOSA``
+                1. ``VGOSA``
+                1. ``MDT``
+                1. ``UGOS``
+                1. ``VGOS``
+        inplace: bool (True)
+            Should predictions be added to the input dataset (Default) or not.
+
+        Returns
+        -------
+        y: :class:`xarray.DataSet`
+            Dataset with OSnet predictions of temperature, salinity profiles and Mixed Layer Depth
+        """
+        if self._is_signed(x):
+            raise ValueError("Cannot make predictions from a dataset that already has OSnet variables")
+
+        y = self._predict(x=x,
+                            ensemble=self.models,
+                            scal_Sm=self.scalers['scal_Sm'], scal_Sstd=self.scalers['scal_Sstd'],
+                            scal_Tm=self.scalers['scal_Tm'], scal_Tstd=self.scalers['scal_Tstd'],
+                            scaler_input=self.scalers['scaler_input'],
+                            suffix="_TS",
+                            **kwargs)
+
+        # ds_output has ds_inputs variables dimensions broadcasted, so we need to re-assign to their original shape:
+        for v in x.data_vars:
+            if v in y:
+                y = y.assign({v: x[v]})
+
+        # Return dataset:
+        if inplace:
+            # Add predicted fields to input dataset:
+            # log.debug("INPLACE: Add predicted fields to input dataset:")
+            # log.debug(list(set(y.data_vars) - set(x.data_vars)))
+            for v in list(set(y.data_vars) - set(x.data_vars)):
+                x = x.assign({v: y[v]})
+            out = x
+        else:
+            # Remove input arrays from the predicted dataset:
+            # log.debug("NOT INPLACE: Remove input arrays from the predicted dataset:")
+            # log.debug(list(set(x.data_vars)))
+            y = y.drop_vars(list(set(x.data_vars)))
+            out = y
+
+        return self._sign_predictions(out)
