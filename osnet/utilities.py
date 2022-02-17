d51e5542ab80893892b28892883e7f73852e09c8
Guillaume Maze <gmaze@ifremer.fr>
2022-02-17 13:52:00 +0100

    Update utilities.py
    docstrings

--- osnet/utilities.py
+++ osnet/utilities.py
@@ -16,6 +16,13 @@ def add_MDT(ds: xr.Dataset, path=None) -> xr.Dataset:
 
     This function is used by the facade to complement an input dataset with missing variables like MDT
 
+    By default, the MDT_CNES_CLS18 Mean Dynamic Topography is interpolated on the lat/lon of the dataset.
+
+    The horizontal resolution of the MDT is: 1/8 in latitude and longitude.
+
+    The MDT_CNES_CLS18 Mean Dynamic Topography is calculated from the combination of altimetry, gravimetry
+    (including GOCE and GRACE) and in-situ data. The reference time-period is 1993-2012.
+
     Parameters
     ----------
     ds: :class:`xarray.DataSet`
@@ -56,6 +63,10 @@ def add_BATHY(ds: xr.Dataset, path=None) -> xr.Dataset:
     """ Add BATHY variable to dataset
 
     This function is used by the facade to complement an input dataset with missing variables like BATHY
+
+    By default, the ETOPO1_Ice_g_gmt4 bathymetry is interpolated on the lat/lon of the dataset.
+
+    The horizontal resolution of the bathymetry is: 1/60 in latitude and longitude.
 
     Parameters
     ----------
@@ -98,6 +109,21 @@ def check_and_complement(ds: xr.Dataset) -> xr.Dataset:
 
     This function is used by the facade when asked to make a prediction
 
+    Here we check of the input :class:`xarray.DataSet` has the required variables to make a prediction:
+        1. ``lat``
+        1. ``lon``
+        1. ``time`` or ``dayOfYear``
+        1. ``BATHY``
+        1. ``MDT``
+        1. ``SST``
+        1. ``SLA``
+        1. ``UGOSA``
+        1. ``VGOSA``
+        1. ``UGOS``
+        1. ``VGOS``
+
+    If ``BATHY``, ``MDT`` or ``dayOfYear`` are missing, we try to add them internally.
+
     Parameters
     ----------
     ds: :class:`xarray.DataSet`
@@ -107,7 +133,6 @@ def check_and_complement(ds: xr.Dataset) -> xr.Dataset:
     -------
     ds: :class:`xarray.DataSet`
         Dataset suitable for OSnet predictions
-
     """
     added = []
     if "BATHY" not in ds.data_vars:
@@ -138,5 +163,6 @@ def check_and_complement(ds: xr.Dataset) -> xr.Dataset:
 
     if len(added) > 0:
         ds.attrs['OSnet-added'] = ";".join(added)
+        # This will be used by the facade to possible delete these variables from the output dataset
 
     return ds
